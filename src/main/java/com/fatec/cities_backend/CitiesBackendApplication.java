af1cae9ba872bc956b6d3ccc9428da1eba1a4c70
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-10-31 19:22:51 -0300

    first commit

---
+++ src/main/java/com/fatec/cities_backend/CitiesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.cities_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitiesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitiesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/cities_backend/CitiesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.cities_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
