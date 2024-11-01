159cfda9ccaa9921b62b3ebe5832fe78557088c5
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-10-31 21:17:18 -0300

    :sparkles: feat: Added the PUT method

--- src/main/java/com/fatec/cities_backend/resources/CityController.java
+++ src/main/java/com/fatec/cities_backend/resources/CityController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +52,11 @@ public class CityController {
     public ResponseEntity<Void> deleteCity(@PathVariable long id){
         cityService.delete(id);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> putCity(@PathVariable long id, @RequestBody CityRequest city){
+        cityService.put(id, city);
+        return ResponseEntity.ok().build();
     }
 }
