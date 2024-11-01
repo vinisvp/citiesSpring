4de319ef015109ebf2f7ca1a468e8aacd8a41f13
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-10-31 21:12:03 -0300

    :sparkles: feat: Added the DELETE method

--- src/main/java/com/fatec/cities_backend/resources/CityController.java
+++ src/main/java/com/fatec/cities_backend/resources/CityController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,11 @@ public class CityController {
                        .toUri();
         
         return ResponseEntity.created(location).body(newCity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCity(@PathVariable long id){
+        cityService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
